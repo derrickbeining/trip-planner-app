fe22c38e38a8ea7a5ea0be81d4bc74e99038a6a7
derrickbeining <derrick.beining@gmail.com>
2017-08-11 18:18:31 -0400

    more setup done, last commit for class day

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
-const express = require('express');
-const Router = require('./routes');
+const express = require( 'express' );
+const Router = require( './routes' );
 const app = express();
-const db = require('./models');
-const nunjucks = require('nunjucks');
+const db = require( './models' );
+const nunjucks = require( 'nunjucks' );
 
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const path = require('path');
+const morgan = require( 'morgan' );
+const bodyParser = require( 'body-parser' );
+const path = require( 'path' );
 
 
-app.set('view engine', 'html');
-app.engine('html', nunjucks.render);
-nunjucks.configure('views', { noCache: true });
+app.set( 'view engine', 'html' );
+app.engine( 'html', nunjucks.render );
+nunjucks.configure( 'views', { noCache: true } );
 
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(Router);
-app.use(function (err, req, res, next) {
-  console.log(err);
-  res.render('error');
-});
+app.use( morgan( 'dev' ),
+  bodyParser.json(),
+  bodyParser.urlencoded( { extended: true } ),
+  express.static( path.join( __dirname, '/public' ) ),
+  express.static( path.join( __dirname, '/node_module/jquery/dist' ) ),
+  express.static( path.join( __dirname, '/node_modules/bootstrap/dist' ) ),
+  Router,
+  function ( err, req, res, next ) {
+    console.log( err )
+    res.render( 'error' )
+  } );
 
 const PORT = 3000;
 
 db.sync()
-.then(() => {
-  console.log('db synced!');
-  app.listen(PORT, function () {
-    console.log(`server listening on port ${PORT}`);
-  });
-});
+  .then(() => {
+    console.log( 'db synced!' );
+    app.listen( PORT, function () {
+      console.log( `server listening on port ${PORT}` );
+    } );
+  } );
 
 //module.exports = app;
